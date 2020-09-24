c9d318e509e5c59fc5dd0c0bce3ccbdc0bd09d4b
Ryan Alencar <ryanalencarbarbosa1701@gmail.com>
2020-09-24 15:35:39 -0300

    feat: main page styled

--- src/pages/Main/index.js
+++ src/pages/Main/index.js
@@ -1,9 +1,25 @@
 import React from 'react';
+import { FaGithubAlt, FaPlus } from 'react-icons/fa';
 
-import { Title } from './styles';
+import { Container, Form, SubmitButton } from './styles';
 
 function Main() {
-  return <Title>Hello World</Title>;
+  return (
+    <Container>
+      <h1>
+        <FaGithubAlt />
+        Repositórios
+      </h1>
+
+      <Form onSubmit={() => {}}>
+        <input type="text" placeholder="Adicionar repositório" />
+
+        <SubmitButton disabled>
+          <FaPlus color="#fff" size={14} />
+        </SubmitButton>
+      </Form>
+    </Container>
+  );
 }
 
 export default Main;
